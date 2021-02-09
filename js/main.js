802da43cf150de3058f684fb3f6eb0751dc9b310
zakeyah <mom11omar@gmail.com>
2021-02-09 19:19:52 +0200

    good

--- js/main.js
+++ js/main.js
@@ -1,39 +1,45 @@
 'use strict';
-let userName = prompt('what\'s your name ?');
+ let score = 0
+
+
+ let userName = prompt('what\'s your name ?');
 
 console.log(userName)
 
 let age = prompt('Do you think that i\'m 90 years old ?');
-if(age.toLowerCase() === 'yes'){
+if(age.toLowerCase() === 'yes'|| age.toLowerCase() === 'y'){
   alert('how could you!!!!');
-}else if(age.toLowerCase() === 'no'){
+}else if(age.toLowerCase() === 'no' || age.toLowerCase() === 'n'){
   alert('yep i am not');
+  score += 1
   console.log('right answer');
 };
 
 
-
 let myGirls = prompt('My tow girls name is rama and rema ?');
-if(myGirls.toLowerCase() ==='yes'){
+if(myGirls.toLowerCase() ==='yes' || myGirls.toLowerCase() ==='y'){
    alert('nooo, they are gazl and laten');
-  }else if(myGirls.toLocaleLowerCase() ==='no'){
+  }else if(myGirls.toLocaleLowerCase() ==='no'|| myGirls.toLowerCase() ==='n'){
     alert('they are gazl and laten');
+    score += 1
     console.log('right answer');
   };
 
   let favColor = prompt('My favorite color is black  ?');
-if(favColor.toLowerCase() ==='yes'){
+if(favColor.toLowerCase() ==='yes' || favColor.toLowerCase() ==='y'){
    alert('what can i say it\'s just a beautiful color');
+   score += 1
    console.log('right answer');
-  }else if(favColor.toLowerCase() ==='no'){
+  }else if(favColor.toLowerCase() ==='no' || favColor.toLowerCase() ==='n'){
     alert('wrong answer , it will always be black');
   };
 
   let traveled = prompt('Did i traveled 10 times in my life  ?');
-if (traveled.toLowerCase() === 'yes'){
+if (traveled.toLowerCase() === 'yes' || traveled.toLowerCase() === 'y'){
    alert('I wish');
-  }else if(traveled.toLowerCase() ==='no'){
+  }else if(traveled.toLowerCase() ==='no'|| traveled.toLowerCase() === 'n'){
     alert('No,but i hope i will travel more than 10 times in the future');
+    score += 1
     console.log('right answer');
   };
   
@@ -42,9 +48,64 @@ if(Season.toLowerCase() ==='yes'){
   alert('No ,winter is the worst');
 }else if(Season.toLowerCase() ==='no'){
   alert('yes i hate winter');
+  score += 1
   console.log('right answer');
   };
 
 
+
+let attempts = 4;
+let userInput = prompt('Can you guess how many months i worked in SOS ?');
+
+while (attempts) {
+  
+  if (userInput === "9") {
+    alert('correct');
+    score += 1;
+    break;  
+  }else if(userInput < "9"){
+    alert('too low.');
+  }else if(userInput > "9"){
+    alert('too high.');
+  }else{
+    alert('th')
+  }
+
+  attempts -= 1;
+  userInput = prompt('please try again you have : ' + attempts + ' remaining!');
+}
+
+
+let placesVisit=['italy','france','rome','greece','egypt','turkey','spain','ukraine','morocco','dubai'];
+
+let chancess=6;
+let userGuess=prompt('guess one of the places i want to visit ');
+while(chancess){
+ let correct=false;
+  for (let i= 0;i<placesVisit.length;i++){
+
+    if (userGuess.toLowerCase() === placesVisit[i]){
+      alert('correct answer!');
+      score += 1;
+       correct=true;
+      break;
+    }
+
+  }
+  if(correct=true){
+    break;
+  }
+  chancess-=1;
+  userGuess=prompt('please try again you have : '+chancess+' remaining!');
+
+
+
+  if (chancess === 1){
+    break;
+  }
+
+}
+
 alert('welcom '+userName)
-console.log(userName,age,myGirls,favColor,traveled,Season)
+alert('your score is '+score)
+//console.log(userName,age,myGirls,favColor,traveled,Season)
